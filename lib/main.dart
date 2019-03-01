856b4d9fe7b8bb7de151a9f153a2a73a84f588f1
Kudo Phạm Quốc Huy <phamquochuy24082013@gmail.com>
2019-03-01 22:13:15 +0700

    [App][Login][đã làm giao diện, xử lí kiểm tra tài khoản mật khẩu]

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,219 @@
 import 'package:flutter/material.dart';
+import 'package:groovin_material_icons/groovin_material_icons.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        debugShowCheckedModeBanner: false,
+        home: LoginPage(),
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+class LoginPage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _LoginPage();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _LoginPage extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _email, _password;
+  bool _isobscuretext = true;
+  Color _coloreye = Colors.grey;
+  _Buildtitle() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text("Login",
+          style: TextStyle(
+            fontSize: 42.0,
+          )),
+    );
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  _BuildTitleLine() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Container(
+          width: 40.0,
+          height: 1.8,
+          color: Colors.black,
+        ),
+      ),
+    );
+  }
+
+  _BuildEmail() {
+    return TextFormField(
+      onSaved: (str) {
+        _email = str;
+      },
+      validator: (str) {
+        if (str.isEmpty || !str.contains("@gmail.com")) {
+          return 'Please enter an email';
+        }
+      },
+      autovalidate: true,
+      decoration: InputDecoration(labelText: "Email Address"),
+    );
+  }
+
+  _BuildPassWord() {
+    return TextFormField(
+      onSaved: (str) {
+        _email = str;
+      },
+      validator: (str) {
+        if (str.isEmpty || str.length <= 5) {
+          return 'Please enter an password';
+        }
+      },
+      autovalidate: true,
+      //onFieldSubmitted: ,
+      decoration: InputDecoration(
+        labelText: "Password",
+        suffixIcon: IconButton(
+          onPressed: () {
+            if (_isobscuretext == true) {
+              setState(() {
+                _coloreye = Colors.blue;
+                _isobscuretext = false;
+              });
+            } else {
+              setState(() {
+                _coloreye = Colors.grey;
+                _isobscuretext = true;
+              });
+            }
+          },
+          icon: Icon(Icons.remove_red_eye),
+          color: _coloreye,
+        ),
+      ),
+      obscureText: _isobscuretext,
+    );
+  }
+
+  _BuildForgotPassWord() {
+    return Padding(
+        padding: EdgeInsets.only(top: 10.0, right: 5.0),
+        child: Align(
+          alignment: Alignment.bottomRight,
+          child: InkWell(
+            child: Text("Forgot Password?"),
+            onTap: () {},
+          ),
+        ));
+  }
+
+  _BuildButtonLogin() {
+    return Align(
+      child: SizedBox(
+        height: 50.0,
+        width: 250.0,
+        child: FlatButton(
+          onPressed: () {
+            if (_formKey.currentState.validate()) {
+              _formKey.currentState.save();
+            }
+          },
+          color: Colors.blueAccent,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Text(
+            "LOGIN",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+        body: Form(
+      key: _formKey,
+      child: ListView(
+        padding: const EdgeInsets.fromLTRB(22.0, 0.0, 22.0, 22.0),
+        children: <Widget>[
+          SizedBox(
+            height: kToolbarHeight,
+          ),
+          _Buildtitle(),
+          _BuildTitleLine(),
+          SizedBox(
+            height: 70,
+          ),
+          _BuildEmail(),
+          _BuildPassWord(),
+          _BuildForgotPassWord(),
+          SizedBox(
+            height: 50,
+          ),
+          _BuildButtonLogin(),
+          SizedBox(
+            height: 30,
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Text("or login with"),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  width: 80.0,
+                  height: 30.0,
+                  decoration: BoxDecoration(
+                      color: Colors.redAccent,
+                      borderRadius: new BorderRadius.circular(50.0)),
+                  //color: Colors.red,
+                  child: Icon(
+                    GroovinMaterialIcons.google,
+                    color: Colors.white,
+                    size: 20.0,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 10.0,
+              ),
+              InkWell(
+                child: Container(
+                  width: 80.0,
+                  height: 30.0,
+                  color: Colors.blueAccent,
+                  child: Icon(
+                    GroovinMaterialIcons.facebook,
+                    color: Colors.white,
+                    size: 20.0,
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    ));
   }
 }
