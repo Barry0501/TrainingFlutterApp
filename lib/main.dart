1aa5bc244fb869cf2849937e5fe948df56c14e60
Kudo Phạm Quốc Huy <phamquochuy24082013@gmail.com>
2019-03-02 12:17:36 +0700

    [App][Calculator][đã hoàn thành giao diện, chức năng cộng trừ nhân chia]

--- lib/main.dart
+++ lib/main.dart
@@ -7,58 +7,162 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Calculator(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+class Calculator extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _Calculator();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _Calculator extends State<Calculator> {
+  String output = "0";
+  double num1 = 0.0;
+  double num2 = 0.0;
+  String outputtemp = "0";
+  String oper = ""; // toán tử
+  _BuildButton(String text) {
+    return OutlineButton(
+      onPressed: () {
+        if (text == "AC") {
+          // clear text
+          oper = ""; // toàn tử
+          num1 = 0.0;
+          num2 = 0.0;
+          outputtemp = "0";
+        } else if (text == "+" || text == "-" || text == "*" || text == "/") {
+          num1 = double.parse(output);
+          oper = text;
+          outputtemp = "0";
+        } else if (text == ".") {
+          if (text.contains(".")) {
+            return;
+          } else {
+            outputtemp = outputtemp + text;
+          }
+        } else if (text == "=") {
+          num2 = double.parse(output);
+          if (oper == "+") {
+            outputtemp = (num1 + num2).toString();
+          }
+          if (oper == "-") {
+            outputtemp = (num1 - num2).toString();
+          }
+          if (oper == "*") {
+            outputtemp = (num1 * num2).toString();
+          }
+          if (oper == "/") {
+            outputtemp = (num1 / num2).toString();
+          }
+          // sau khi cộng reset về trạn thái ban đầu
+          num1 = 0.0;
+          num2 = 0.0;
+          oper = "";
+        } else {
+          outputtemp = outputtemp + text;
+        }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+        setState(() {
+          output = double.parse(outputtemp).toString();
+        });
+      },
+      //color: Colors.white,
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Colors.black,
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Calculator"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 2,
+            alignment: Alignment.bottomRight,
+            padding: new EdgeInsets.symmetric(
+              vertical: 24.0,
+              horizontal: 12.0,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            child: new Text(output,
+                style: TextStyle(
+                  fontSize: 48.0,
+                  fontWeight: FontWeight.bold,
+                )),
+          ),
+          Divider(),
+          Container(
+            color: Colors.teal,
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    _BuildButton("AC"),
+                    _BuildButton("C"),
+                    _BuildButton("%"),
+                    _BuildButton("/"),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    _BuildButton("7"),
+                    _BuildButton("8"),
+                    _BuildButton("9"),
+                    _BuildButton("*"),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    _BuildButton("4"),
+                    _BuildButton("5"),
+                    _BuildButton("6"),
+                    _BuildButton("-"),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    _BuildButton("1"),
+                    _BuildButton("2"),
+                    _BuildButton("3"),
+                    _BuildButton("+"),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Expanded(
+                      //flex: 2,
+                      child: _BuildButton("0"),
+                    ),
+                    _BuildButton("."),
+                    _BuildButton("="),
+                  ],
+                )
+              ],
             ),
-          ],
-        ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
