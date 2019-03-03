a486d8c6d477d5e3707696cae0c372459e907379
Kudo Phạm Quốc Huy <phamquochuy24082013@gmail.com>
2019-03-03 10:54:03 +0700

    [App][Appchat][Hoàn thành giao diện xử lí chat]

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,116 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData.light(),
+      home: AppChat(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+class AppChat extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _AppChat();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+class _AppChat extends State<AppChat> {
+  final _listchatmesseage = new List();
+  TextEditingController _textController = new TextEditingController();
+  void _handleSubmitted(String text) {
+    ChatMesseage listms = new ChatMesseage(
+      text: text,
+    );
+    _textController.clear();
     setState(() {
-      _counter++;
+      _listchatmesseage.insert(0, listms);
     });
+  }
+
+  _buildTextCompose() {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: TextField(
+        controller: _textController,
+        onSubmitted: _handleSubmitted,
+        decoration: InputDecoration(
+            hintText: 'Nhập tin nhắn',
+            suffixIcon: Icon(
+              Icons.send,
+            )),
+        onTap: () {
+          setState(() {
+            _listchatmesseage.insert(
+                0, new ChatMesseage(text: _textController.text));
+            _textController.clear();
+          });
+        },
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      appBar: new AppBar(
+        title: Text('App chat'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: new Column(
+        children: <Widget>[
+          new Flexible(
+            child: new ListView.builder(
+              padding: new EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (context, index) => _listchatmesseage[index],
+              itemCount: _listchatmesseage.length,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+          ),
+          _buildTextCompose(),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class ChatMesseage extends StatelessWidget {
+  String text;
+  ChatMesseage({this.text});
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: EdgeInsets.all(10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text("H"),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('Huy dz',style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),),
+                SizedBox(height: 10.0,),
+                Text(this.text),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
