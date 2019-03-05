c717aa283e4f90f4db9442a2bf7e04558722b2eb
Kudo Phạm Quốc Huy <phamquochuy24082013@gmail.com>
2019-03-05 19:48:33 +0700

    [App][Randomword][chức năng click từ sau đó chuyển sang page khác]

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+class Home extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _Home();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _Home extends State<Home> {
+  // final word = WordPair.random();
+  Color _color = Colors.grey;
+  final listword = <WordPair>[];
+  final Set<WordPair> flagword = new Set<WordPair>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Startup name Generator'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: _pushSaved,
+          )
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      body: Container(
+        //color: Colors.grey,
+        child: ListView.builder(
+          //itemCount: listword.length,
+          itemBuilder: (context, index) {
+            if (index >= listword.length) {
+              listword.addAll(generateWordPairs().take(10));
+            }
+            return _buildWord(listword[index], index);
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = flagword.map(
+            (WordPair pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+          return new Scaffold(
+            // Add 6 lines from here...
+            appBar: new AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: new ListView(children: divided),
+          ); // ... to here.
+        },
+      ),
+    );
+  }
+
+  _buildWord(WordPair word, int index) {
+    final check = flagword.contains(word);
+    return ListTile(
+      title: Text(
+        word.toString(),
+        //word.asUpperCase,
+        style: TextStyle(
+          color: Colors.black,
+        ),
+      ),
+      onTap: () {
+        print('click dòng ' + index.toString());
+        setState(() {
+          if (check) {
+            flagword.remove(word);
+          } else {
+            flagword.add(word);
+          }
+        });
+      },
+      trailing: Icon(
+        Icons.star,
+        color: check ? Colors.red : null,
+      ),
     );
   }
 }
